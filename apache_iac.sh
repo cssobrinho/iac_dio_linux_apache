fc5cd8289cda1bd6c6b9961639a393788289efcc
cssobrinho <cssobrinho2014@gmail.com>
2022-07-23 16:26:09 -0300

    Apache2 v.1.0

---
+++ apache_iac.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Provisionamento para servidor web Apache!"
+
+echo "Atualizando o servidor..."
+apt-get update
+apt-get upgrade -y
+
+echo "Instalando o Apache"
+apt-get install apache2 -y
+
+echo "Instalando o Unzip"
+apt-get unzip -y
+
+echo "Baixando e copiando os arquivos da aplicação - Font gitHub"
+
+cd /tmp
+
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+
+cd linux-site-dio-main
+
+cp -R * /var/www/html/
